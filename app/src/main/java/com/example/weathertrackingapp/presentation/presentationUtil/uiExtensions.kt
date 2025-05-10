691172ed532f181f734f829fa12af5d1d1b285ae
Mahmoud Khairy <mahmoud5airy2000@gmail.com>
2025-05-10 21:21:03 +0300

    added some important method and interface documentations

--- app/src/main/java/com/example/weathertrackingapp/presentation/presentationUtil/uiExtensions.kt
+++ app/src/main/java/com/example/weathertrackingapp/presentation/presentationUtil/uiExtensions.kt
@@ -3,6 +3,14 @@ package com.example.weathertrackingapp.presentation.presentationUtil
 import android.view.View
 import android.widget.TextView
 
+/**
+ * Extension function for `TextView` to bind text or hide it based on the value.
+ *
+ * If the provided value is `null` or blank, the `TextView`'s visibility will be set to `INVISIBLE`.
+ * If the value is not `null` or blank, it will set the `TextView`'s text to the value and make it visible.
+ *
+ * @param value The string value to bind to the `TextView`. If `null` or blank, the `TextView` will be hidden.
+ */
 fun TextView.bindOrHide(value: String?) {
     if (value.isNullOrBlank()) {
         visibility = View.INVISIBLE
